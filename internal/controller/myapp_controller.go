01a4c0d87c189518a211f3d592cd80185748211f
zhangwei <wei.zhang@iwellmass.com>
2023-11-09 17:07:48 +0800

    add   contro

--- internal/controller/myapp_controller.go
+++ internal/controller/myapp_controller.go
@@ -18,19 +18,23 @@ package controller
 
 import (
 	"context"
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	timmyv1 "oprator3/api/v1"
+	v1 "oprator3/api/v1"
 )
 
 // MyAppReconciler reconciles a MyApp object
 type MyAppReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	Log    logr.Logger
 }
 
 //+kubebuilder:rbac:groups=timmy.timmy.com,resources=myapps,verbs=get;list;watch;create;update;patch;delete
@@ -51,12 +55,30 @@ func (r *MyAppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 
 	// TODO(user): your logic here
 
+	//用于在日志记录器中添加键值对的上下文信息。在这个例子中，"myapp" 是键，req.NamespacedName 是值。req.NamespacedName的值是一个 "命名空间/资源对象名称"的组合
+	logger := r.Log.WithValues("myapp", req.NamespacedName)
+	logger.Info("Reconciling myapp")
+
+	//判断MyApp对象是否存在
+	instance := &v1.MyApp{}
+	if err := r.Client.Get(ctx, req.NamespacedName, instance); err != nil {
+		if errors.IsNotFound(err) {
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{}, err
+	}
+
+	logger.Info("app kind: " + instance.Kind + ", app name: " + instance.Name)
+	if instance.DeletionTimestamp != nil {
+		return reconcile.Result{}, nil
+	}
+
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *MyAppReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&timmyv1.MyApp{}).
+		For(&v1.MyApp{}).
 		Complete(r)
 }
